5e1ae9031aff01f740b80b1ff2e0d0d5b58f8902
Riukkon <raphafm.rf@gmail.com>
2020-06-02 14:53:07 -0300

    commit - day 2

---
+++ scripts/intro.js
@@ -0,0 +1,63 @@
+// comments
+// document.write("Hello")
+
+// let myvar = "Hello"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso é uma string'
+// `Isso é uma string`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bFalse)
+
+// objeto
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("Executar uma grande lógica de correr")
+//     // return "run Forest"
+//   }
+// }
+
+// document.write(pessoa.correr())
+// pessoa.correr()
+
+// Arrays
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// funções
+// function sayMyName(nome){
+//   document.write(nome)
+// }
+
+// sayMyName("Raphael")
+
+// condicionais
+// const notaFinal = 7
+
+// if (notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// laços de repetição
+// for (i = 0; i < 10; i++) {
+//   document.write(`<span>${i+1}</span><br>`)
+// }
